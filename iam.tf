b2033be6533c3fe6b331219cc0e8893307fa5ea4
dinindunz <31271518+dinindunz@users.noreply.github.com>
2025-08-04 20:14:00 +1200

    Add Lambda IAM role with SNS publish permission

---
+++ iam.tf
@@ -0,0 +1,38 @@
+resource "aws_iam_role" "lambda_role" {
+  name = "notification-service-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "lambda_sns_policy" {
+  name = "lambda-sns-publish-policy"
+  role = aws_iam_role.lambda_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = "sns:Publish"
+        Resource = "arn:aws:sns:ap-southeast-2:722141136946:user-notifications"
+      }
+    ]
+  })
+}
+
+# Basic Lambda execution policy for CloudWatch Logs
+resource "aws_iam_role_policy_attachment" "lambda_basic" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
